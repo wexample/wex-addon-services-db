3c47c5b7f3957c6b6a27609d85f7e40b7e7c7604
weeger <romain.weeger@gmail.com>
2023-01-26 09:58:11 +0100

    Code format

--- services/maria-10/hooks/appConfig.sh
+++ services/maria-10/hooks/appConfig.sh
@@ -13,7 +13,7 @@ maria10AppConfig() {
   MARIA_DB_NAME=${MARIA_DB_NAME:-${NAME}}
   MARIA_DB_PASSWORD=${MARIA_DB_PASSWORD:-${WEX_DEFAULT_INSECURE_PASSWORD}}
   MARIA_DB_PORT=${MARIA_DB_PORT:-3306}
-    MARIA_DB_USER=${MARIA_DB_USER:-"root"}
+  MARIA_DB_USER=${MARIA_DB_USER:-"root"}
 
   wex app::config/setValue -k=MYSQL_DB_HOST -v="${MARIA_DB_HOST}"
   wex app::config/setValue -k=MYSQL_DB_NAME -v="${MARIA_DB_NAME}"
@@ -21,17 +21,16 @@ maria10AppConfig() {
   wex app::config/setValue -k=MYSQL_DB_PORT -v="${MARIA_DB_PORT}"
   wex app::config/setValue -k=MYSQL_DB_USER -v="${MARIA_DB_USER}"
 
-
   _wexLog "Maria : creating cnf file"
   # Create connexion file info
   local DB_CONNECTION_FILE="${WEX_DIR_APP_TMP}mysql.cnf"
 
   touch "${DB_CONNECTION_FILE}"
   chmod 755 "${DB_CONNECTION_FILE}"
-  echo '[client]' > "${DB_CONNECTION_FILE}"
-  echo 'user = "'${MARIA_DB_USER}'"' >> "${DB_CONNECTION_FILE}"
-  echo 'password = "'${MARIA_DB_PASSWORD}'"' >> "${DB_CONNECTION_FILE}"
-#  echo 'host = "'${MARIA_DB_HOST}'"' >> "${DB_CONNECTION_FILE}"
+  echo '[client]' >"${DB_CONNECTION_FILE}"
+  echo 'user = "'${MARIA_DB_USER}'"' >>"${DB_CONNECTION_FILE}"
+  echo 'password = "'${MARIA_DB_PASSWORD}'"' >>"${DB_CONNECTION_FILE}"
+  #  echo 'host = "'${MARIA_DB_HOST}'"' >> "${DB_CONNECTION_FILE}"
 
   # Expected access level
   chmod 644 "${DB_CONNECTION_FILE}"
