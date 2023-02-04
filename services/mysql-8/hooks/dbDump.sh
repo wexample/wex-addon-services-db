57d164ea721818c8af27647059d4bb7a2f843314
weeger <romain.weeger@gmail.com>
2023-02-04 16:41:55 +0100

    Fix dump

--- services/mysql-8/hooks/dbDump.sh
+++ services/mysql-8/hooks/dbDump.sh
@@ -5,17 +5,12 @@ mysql8DbDump() {
 
   local DUMP_NAME="${1}"
   local SERVICE_DIR="$(wex app::service/dir -s="mysql-8")"
-  DUMP_FILE_NAME+=".sql"
+  DUMP_FILE_NAME="${DUMP_NAME}.sql"
 
   # shellcheck source=dbConnect.sh
   source "${SERVICE_DIR}hooks/dbConnect.sh"
-  # shellcheck source=.wex/tmp/config.build
-  source "${WEX_FILEPATH_REL_CONFIG_BUILD}"
 
   wex app/exec -n="${DB_CONTAINER}" -c="mysqldump $(mysql8DbConnect) ${APP_NAME} > /var/www/dumps/${DUMP_FILE_NAME}"
 
-  # Seems some dumps files take a while to be accessible.
-  sleep 2
-
-  echo "${WEX_DIR_APP_DATA}${DB_CONTAINER}/dumps/${DUMP_NAME}"
+  echo "${WEX_DIR_APP_DATA}${DB_CONTAINER}/dumps/${DUMP_FILE_NAME}"
 }
