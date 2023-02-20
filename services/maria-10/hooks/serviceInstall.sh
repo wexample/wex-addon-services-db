2774bdd1395fd66f50d32c8d42ef69dbe53c0f15
weeger <romain.weeger@gmail.com>
2023-02-20 21:12:02 +0100

    Recreate app for each tested service

--- services/maria-10/hooks/serviceInstall.sh
+++ services/maria-10/hooks/serviceInstall.sh
@@ -3,5 +3,5 @@
 maria10ServiceInstall() {
   wex-exec app::config/setValue -b -k=DB_CONTAINER -v=maria
   wex-exec app::config/setValue -b -k=MARIA_VERSION -v=10.9
-  wex-exec app::config/setValue -k=MARIA_DB_PASSWORD -v="${WEX_DEFAULT_INSECURE_PASSWORD}"
+  wex-exec app::config/setValue -b -k=MARIA_DB_PASSWORD -v="${WEX_DEFAULT_INSECURE_PASSWORD}"
 }
