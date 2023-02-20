d23fe001534b4009373ef37522b3175547500122
weeger <romain.weeger@gmail.com>
2023-02-20 17:24:54 +0100

    Use wex-exec

--- services/mongo-6/hooks/appInit.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mongo6AppInit() {
-  wex app::config/setValue -b -k=MONGO_VERSION -v=6.0.3
-}
--- services/mongo-6/hooks/serviceInstall.sh
+++ services/mongo-6/hooks/serviceInstall.sh
@@ -2,4 +2,5 @@
 
 mongo6ServiceInstall() {
   wex app::config/setValue -b -k=DB_CONTAINER -v=mongo
+  wex app::config/setValue -b -k=MONGO_VERSION -v=6.0.3
 }
