f45267a58878b3d76873d21aea8c8d80aca4496f
weeger <romain.weeger@gmail.com>
2023-02-20 17:09:59 +0100

    Rename function wexRun to wex-exec && run tests in services && first db test

--- services/maria-10/hooks/serviceInstall.sh
+++ services/maria-10/hooks/serviceInstall.sh
@@ -2,4 +2,6 @@
 
 maria10ServiceInstall() {
   wex app::config/setValue -b -k=DB_CONTAINER -v=maria
+  wex app::config/setValue -b -k=MARIA_VERSION -v=10.9
+  wex app::config/setValue -k=MARIA_DB_PASSWORD -v="${WEX_DEFAULT_INSECURE_PASSWORD}"
 }
---
+++ services/maria-10/tests/dbConnect.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+maria10DbConnectTest() {
+  # TODO
+  _wexLog "Test maria DB connexion"
+  _wexTestAssertEqual true true
+}
