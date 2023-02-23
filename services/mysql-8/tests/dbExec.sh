cac52b2ab09574b9e58e9f47ccee5db4310cb093
weeger <romain.weeger@gmail.com>
2023-02-23 17:00:22 +0100

    Fix mysql test

--- services/mysql-8/tests/dbExec.sh
+++ services/mysql-8/tests/dbExec.sh
@@ -3,22 +3,28 @@
 mysql8DbExecTest() {
   wex-exec app::app/start
 
-  _wexLog "Waiting for MySQL to start..."
-  sleep 5
+  local TABLES=()
+  while [[ ${#TABLES[@]} -lt 5 ]]
+  do
+      _wexLog "Waiting for database to be ready..."
+      printf "${TABLES[*]}"
+      sleep 1
 
-  local OUTPUT
-  OUTPUT=($(wex-exec db/exec -c="SHOW DATABASES"))
+      TABLES=($(wex-exec db/exec -c="SHOW DATABASES"))
+  done
 
   _wexLog "Database tables :"
-  printf "${OUTPUT[*]}"
+  printf '%s\n' "${TABLES[@]}"
 
-  local SUCCESS=false
-  if [[ " ${OUTPUT[*]} " =~ " information_schema " ]] && \
-     [[ " ${OUTPUT[*]} " =~ " mysql " ]] && \
-     [[ " ${OUTPUT[*]} " =~ " performance_schema " ]] && \
-     [[ " ${OUTPUT[*]} " =~ " sys " ]]; then
-      SUCCESS=true
-  fi
+  local EXPECTED_TABLES=("information_schema" "mysql" "performance_schema" "sys")
+  local SUCCESS=true
+
+  for EXPECTED_TABLE in "${EXPECTED_TABLES[@]}"; do
+    if ! printf '%s\n' "${TABLES[@]}" | grep -q -w "$EXPECTED_TABLE"; then
+      SUCCESS=false
+      break
+    fi
+  done
 
   _wexTestAssertEqual "${SUCCESS}" "true"
 
