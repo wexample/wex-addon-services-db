14b584431b2bf6b6d1a4ffe54ebd5cfe9738d7b8
weeger <romain.weeger@gmail.com>
2023-02-25 09:33:28 +0100

    Code reformat

--- services/postgres/sql/dbDrop.sql
+++ services/postgres/sql/dbDrop.sql
@@ -1,7 +1,8 @@
-
 BEGIN;
 REVOKE CONNECT ON DATABASE :db_name FROM public;
-SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = :db_name_escaped;
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = :db_name_escaped;
 COMMIT;
 DROP DATABASE IF EXISTS :db_name;
 
